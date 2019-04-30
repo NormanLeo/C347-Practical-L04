f3800158854d8b4fc09a72db98b07b8517526f92
NormanLeo <17010201@myrp.edu.sg>
2019-04-30 11:50:41 +0800

    Commit

---
+++ L02/DemoImageView/app/src/main/java/sg/edu/rp/c346/demoimageview/ArrayAdapter.java
@@ -0,0 +1,4 @@
+package sg.edu.rp.c346.demoimageview;
+
+public class ArrayAdapter {
+}
---
+++ L02/DemoImageView/app/src/main/java/sg/edu/rp/c346/demoimageview/ListActivity.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346.demoimageview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ListActivity  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+    }
+}
