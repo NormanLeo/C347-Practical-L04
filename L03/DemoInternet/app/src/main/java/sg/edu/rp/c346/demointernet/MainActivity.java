a5e10fcf6e038658655c37c765f68f74535830c7
NormanLeo <17010201@myrp.edu.sg>
2019-05-07 11:57:52 +0800

    Commit

--- L03/DemoInternet/app/src/main/java/sg/edu/rp/c346/demointernet/MainActivity.java
+++ L03/DemoInternet/app/src/main/java/sg/edu/rp/c346/demointernet/MainActivity.java
@@ -1,13 +1,29 @@
 package sg.edu.rp.c346.demointernet;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnRP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnRP = findViewById(R.id.buttonRP);
+        btnRP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent rpIntent = new Intent(Intent.ACTION_VIEW);
+                rpIntent.setData(Uri.parse("http://www.rp.edu.sg"));
+                startActivity(rpIntent);
+            }
+        });
+
     }
 }
